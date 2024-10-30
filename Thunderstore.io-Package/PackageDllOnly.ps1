605b5df9e8302b9b4b0eafc63d83efb28d4ad85c
none <none@example.com>
2024-10-30 11:15:54 -0400

    Package update

--- Thunderstore.io-Package/PackageDllOnly.ps1
+++
@@ -1,3 +0,0 @@
-$ProjectName = "IdleVillager"
-Copy-Item "..\src\bin\Release\netstandard2.0\$ProjectName.dll" -Destination .
-Compress-Archive -Path "$ProjectName.dll" -Force -DestinationPath ./$ProjectName.zip
